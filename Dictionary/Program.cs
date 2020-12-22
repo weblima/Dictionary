4597dfb6b7e3191ce4a75faf8c9a6b07b813a0c6
Rafael Lima <rafael_weblima@hotmail.com>
2020-12-22 17:09:19 -0300

    Implementação do dictionary, lendo array de nomes e votos em um arquivo txt

---
+++ Dictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dictionary {
+    class Program {
+        static void Main(string[] args) {
+
+            Dictionary<string, int> names = new Dictionary<string, int>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votos = int.Parse(line[1]);
+
+                        if (names.ContainsKey(name)) {
+                            names[name] += votos;
+                        }
+                        else {
+                            names[name] = votos;
+                        }
+                        
+                    }
+                }
+
+                foreach (KeyValuePair<string, int> item in names) {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
